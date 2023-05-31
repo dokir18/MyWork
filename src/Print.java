0f098548ff1665fb1679a48737b6512d4e2fc17b
小蔡吖 <3081532125@qq.com>
2023-05-31 22:18:44 +0800

    提取方法2

--- src/Print.java
+++ src/Print.java
@@ -1,10 +1,15 @@
 public class Print {
     public static void main(String[] args) {
         fun1();
+        fun2();
+    }
+
+    private static void fun2() {
+        System.out.println("测试提交");
     }
 
     private static void fun1() {
-        System.out.println("hello word");
+        System.out.println("hello word");//提取方法
     }
 }
 
