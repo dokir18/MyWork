4ade62952d955924c8174d5b96aceefa3e246dd6
小蔡吖 <3081532125@qq.com>
2023-05-31 20:39:44 +0800

    提取方法

--- src/Print.java
+++ src/Print.java
@@ -1,5 +1,10 @@
 public class Print {
     public static void main(String[] args) {
+        fun1();
+    }
+
+    private static void fun1() {
         System.out.println("hello word");
     }
 }
+
